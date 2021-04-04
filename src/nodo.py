f0cda8931d945d87deaaee305ec0c027e70262d9
200011006Adolfo <adolfolopez7435@gmailcom>
2021-04-04 00:41:05 -0600

    Se crearon las siguientes clases nodo para la manipulacion de la matriz ortogonal sus atributos son  fila,columna,dato y posee cuatro apuntadores.
    Se creo el modulo cabecera la cual posee dos clases (nodoCabecera) y 
    (ListaCabecera) 
    ademas los metodos necesarios para su manipulacion
    
    Se creo la clase matriz esta clase nos ayudara para agregar datos a la 
    matriz

---
+++ src/nodo.py
@@ -0,0 +1,9 @@
+class Nodo:
+    def __init__(self,nfila,ncolumna,dato):
+        self.fila=nfila
+        self.columna=ncolumna
+        self.dato=dato
+        self.izquierda=None
+        self.derecha=None
+        self.arriba=None
+        self.abajo=None
